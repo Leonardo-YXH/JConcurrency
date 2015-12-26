9ca6295bbbe27a7aa3d2c95468b4691d3d647d65
Leonardo-YXH <1577168398@qq.com>
2015-12-26 11:20:41 +0800

    代码更新

---
+++ src/chapter1/Main.java
@@ -0,0 +1,21 @@
+package chapter1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Thread task=new PrimeGenerator();
+		task.start();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(!Thread.interrupted()){
+			task.interrupt();
+		}
+		
+	}
+
+}
---
+++ src/chapter1/PrimeGenerator.java
@@ -0,0 +1,32 @@
+package chapter1;
+
+public class PrimeGenerator extends Thread {
+
+	public void run(){
+		long number=1L;
+		while(true){
+			if(isPrime(number)){
+				System.out.printf("Number %d is Prime\n",number);
+			}
+			if(isInterrupted()){
+				System.out.printf("this prime Generator has been Interrupted\n");
+				return;
+			}
+			number++;
+			//System.out.println(number);
+		}
+	}
+	private boolean isPrime(long number) {
+		if (number <=2) {
+			return true;
+			}
+			for (long i=2; i<number; i++){
+			if ((number % i)==0) {
+			return false;
+			}
+			}
+			return true;
+	}
+	
+
+}
